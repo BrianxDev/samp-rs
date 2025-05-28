4c362acb48b7d684b87c13428e13c3b9e4e20751
BrianxDev <phutthathep2006@gmail.com>
2025-05-28 16:01:35 +0700

    Support Thai Language

--- samp-sdk/src/encoding.rs
+++ samp-sdk/src/encoding.rs
@@ -1,8 +1,8 @@
 //! String encoding.
-pub use encoding_rs::{WINDOWS_1251, WINDOWS_1252};
+pub use encoding_rs::{WINDOWS_1251, WINDOWS_1252, WINDOWS_874};
 use encoding_rs::Encoding;
 
-static mut DEFAULT_ENCODING: &Encoding = WINDOWS_1252;
+static mut DEFAULT_ENCODING: &Encoding = WINDOWS_874;
 
 pub fn set_default_encoding(encoding: &'static Encoding) {
     unsafe {
